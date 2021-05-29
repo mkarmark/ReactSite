1d9634049b5c68443900a83f38a49e625c87f7c0
mkarmark <mikarmar@microsoft.com>
2021-05-28 18:39:35 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -1,8 +1,13 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
     context.res = {
-        headers: { 'foobar': 'baz'},
-        body: req.headers
-    }
-};
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
